ec0ff0b340d7fe5098259cb5cc8c37d17e4fa569
Muzammil React(Developer) <79418859+muzammil7912@users.noreply.github.com>
2024-01-25 18:47:57 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,102 @@
+var Swipes = new Swiper(".swiper-container", {
+    speed: 500,
+    effect: "fade",
+    grabCursor: false,
+    slidesPerView: "auto",
+  
+    // autoplay: {
+    //     delay: 5000,
+    //     disableOnInteraction: false,
+    // },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+var Swipes = new Swiper(".swiper-container2", {
+    speed: 500,
+    effect: "slide",
+    grabCursor: false,
+  //   allowTouchMove: false,
+    centeredSlides: true,
+    slidesPerView: "auto",
+  
+    // autoplay: {
+    //     delay: 5000,
+    //     disableOnInteraction: false,
+    // },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+
+  var Swipes2 = new Swiper(".section7__bottomswiper", {
+    //   loop:true,
+    spaceBetween: 40,
+        speed: 500,
+      slidesPerView: 3,
+      grabCursor: false,
+      autoplay: {
+          delay:5000,
+          disableOnInteraction: false,
+      },
+      navigation: {
+        nextEl: ".section7r",
+        prevEl: ".section7l",
+      },
+      breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetweenSlides: 40
+        },
+        979: {
+            slidesPerView: 3,
+            spaceBetweenSlides: 40
+        }
+    }
+    });
+
+
+    $(".yartcategories__top li a").click(function(e) {
+      e.preventDefault()
+      $(".yartcategories__top li a").removeClass("active");
+      $(this).addClass("active");
+    })
+
+    $(".yartcarrental2__top li").click(function(e) {
+      e.preventDefault()
+      $(".yartcarrental2__top li").removeClass("active");
+      $(this).addClass("active");
+    })
+    $(".liens").click(function(e) {
+      e.preventDefault();
+      $(this).toggleClass("active")
+      $(".modalBlack").toggleClass("active")
+      $(".header__left nav").toggleClass("active")
+    })
+    $(".header__left .close").click(function(e) {
+      e.preventDefault();
+      $(".modalBlack").removeClass("active")
+      $(".header__left nav").removeClass("active")
+    })
+      $(".modalBlack").click(function(e) {
+        e.preventDefault();
+        $(".modalBlack").removeClass("active")
+        $(".header__left nav").removeClass("active")
+      })
+
+
+
+      $(window).on('scroll', function() {
+        if ($(this).scrollTop() > 100) {
+            $('.header').addClass('go-top');
+            // $('.logo_side').addClass('hide');
+            // $('.logo_side_2').removeClass('hide');
+        } else {
+            $('.header').removeClass('go-top');
+            // $('.logo_side').removeClass('hide');
+            // $('.logo_side_2').addClass('hide');
+          }
+        });
